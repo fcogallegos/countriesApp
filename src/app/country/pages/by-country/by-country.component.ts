4b3136cb01ce0015dc18f56907027dabd97e2266
Francisco Gallegos <fgallegosgo@gmail.com>
2021-11-12 11:17:13 -0700

    service for create countries

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-country',
@@ -11,11 +12,16 @@ export class ByCountryComponent {
 
   term: string = '';
 
-  constructor() { }
+  constructor( private countryService: CountryService ) { }
 
 
   search() {
     console.log(this.term);
+
+    this.countryService.searchCountry(this.term)
+      .subscribe( resp => {
+        console.log(resp);
+      })
   }
 
 }
