de038425a9b7dadbcad8ec5026c00f6f70157ecb
Francisco Gallegos <fgallegosgo@gmail.com>
2021-11-17 13:29:45 -0700

    Input and Table components

---
+++ src/app/country/components/country-input/country-input.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-input',
+  templateUrl: './country-input.component.html',
+  styles: [
+  ]
+})
+export class CountryInputComponent {
+
+
+  term: string = '';
+
+
+  search() {
+    
+  }
+}
--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -8,6 +8,8 @@ import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
 import { ByCountryComponent } from './pages/by-country/by-country.component';
 import { ByRegionComponent } from './pages/by-region/by-region.component';
 import { SeeCountryComponent } from './pages/see-country/see-country.component';
+import { CountryTableComponent } from './components/country-table/country-table.component';
+import { CountryInputComponent } from './components/country-input/country-input.component';
 
 
 
@@ -18,13 +20,17 @@ import { SeeCountryComponent } from './pages/see-country/see-country.component';
     ByCapitalComponent,
     ByCountryComponent,
     ByRegionComponent,
-    SeeCountryComponent
+    SeeCountryComponent,
+    CountryTableComponent,
+    CountryInputComponent
   ],
   exports: [
     ByCapitalComponent,
     ByCountryComponent,
     ByRegionComponent,
-    SeeCountryComponent
+    SeeCountryComponent,
+    CountryTableComponent
+
   ],
   imports: [
     CommonModule,
