9159c99accd07c313ebd18a2555937ad53cc6def
fcogallegos <fgallegosgo@gmail.com>
2021-11-19 13:04:03 -0700

    for capital

--- src/app/country/pages/by-capital/by-capital.component.ts
+++ src/app/country/pages/by-capital/by-capital.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital',
@@ -6,11 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class ByCapitalComponent implements OnInit {
+export class ByCapitalComponent {
 
-  constructor() { }
+  term: string = '';
+  thereError: boolean = false;
+  capitals: Country[] = [];
+  
+  constructor( private countryService: CountryService  ) { }
 
-  ngOnInit(): void {
+  search( term: string ) {
+    this.thereError = false;
+    this.term = term;
+  
+
+    this.countryService.searchCapital(term)
+      .subscribe( (capitals) => {
+        console.log(capitals);
+        this.capitals = capitals;
+      }, (err) => {
+        this.thereError = true;
+        this.capitals = [];
+      });
   }
 
 }
