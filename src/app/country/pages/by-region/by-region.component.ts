007ccafe919b7701e114d160432bdf857be2910f
fcogallegos <fgallegosgo@gmail.com>
2021-11-25 20:36:29 -0700

    CSS class conditionals

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -15,10 +15,15 @@ import { Component } from '@angular/core';
 export class ByRegionComponent {
 
 
-  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regions: string[] = ['EU','EFTA','CARICOM','PA','AU','USAN','EEU','AL','ASEAN','CAIS'
+                      ,'CEFTA','NAFTA','SAARC'];
   activeRegion: string = '';
 
   constructor() { }
+
+  getClassCSS( region: string ): string {
+    return (region === this.activeRegion) ? 'btn btn-primary':'btn btn-outline-primary';
+  }
 
   activateRegion( region: string ) {
     this.activeRegion = region;
@@ -27,3 +32,5 @@ export class ByRegionComponent {
   }
 
 }
+
+
